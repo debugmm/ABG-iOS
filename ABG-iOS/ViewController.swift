f330929bbec581caff9b7f0bdfb8a1ec28f59385
wujungao <abuglife@qq.com>
2020-03-26 14:39:57 +0800

    proj init:项目初始化

---
+++ ABG-iOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ABG
+//
+//  Created by wujungao on 2020/3/19.
+//  Copyright © 2020 com.wujungao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor=UIColor.green
+                
+        let btn=UIButton.init(type: .detailDisclosure)
+        btn.addTarget(self, action: #selector(btnAction(sender:)), for: .touchUpInside)
+        
+        self.view.addSubview(btn)
+    }
+    
+    @objc func btnAction(sender:UIButton){
+    }
+}
+
